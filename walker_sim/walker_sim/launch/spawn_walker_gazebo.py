1873db17226387dcc51e95253c78a32cadc82cad
aihoque2 <aihoque2@illinois.edu>
2022-12-15 12:07:20 -0800

    create launch.py file

--- walker_sim/walker_sim/launch/spawn_walker_gazebo.py
+++ walker_sim/walker_sim/launch/spawn_walker_gazebo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_package_share_path
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
@@ -16,6 +16,12 @@ def generate_launch_description():
 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_box_car_gazebo = get_package_share_directory('walker_sim')
+    
+    urdf_tutorial_path = get_package_share_path('walker_sim')
+    default_model_path = urdf_tutorial_path / 'robots/simple_walker.urdf'
+
+    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
+                                      description='Absolute path to robot urdf file')
 
     # Gazebo launch
     gazebo = IncludeLaunchDescription(
