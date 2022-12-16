794368b237214ce987826754cc30ae6bdecc7eef
aihoque2 <aihoque2@illinois.edu>
2022-12-16 01:56:02 -0800

    trying to make gazebo launch in ros2

--- walker_sim/walker_sim/launch/walker_gazebo.launch.py
+++ walker_sim/walker_sim/launch/walker_gazebo.launch.py
@@ -31,9 +31,5 @@ def generate_launch_description():
     )    
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-          'world',
-          default_value=[os.path.join(pkg_box_car_gazebo, 'worlds', 'box_bot_empty.world'), ''],
-          description='SDF world file'),
-        gazebo
+        gazebo,
     ])
