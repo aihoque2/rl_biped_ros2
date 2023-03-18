e0a7da84b082a5f388df8041b7e56c951dad836f
aihoque2 <aihoque2@illinois.edu>
2023-03-18 02:14:50 -0700

    delete entity node set up

--- walker_ai/src/delete_entity.py
+++ walker_ai/src/delete_entity.py
@@ -1,8 +1,33 @@
+#!/usr/bin/env python3
+
 import rclpy
 from rclpy.node import Node
 from gazebo_msgs.srv import DeleteEntity, SpawnEntity
 
 class DeleteWalker(Node):
+    """
+    client node
+    """
     def __init__(self):
-        super().__init__("Delete Walker client")
+        super().__init__("delete_walker_client")
+        self.cli = self.create_client(DeleteEntity,"/delete_entity")
+        self.req = DeleteEntity.Request()
+        self.req.name = "bipedal_walker"
+
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
         
+        self.req.name  = "bipedal_walker"
+
+    def send_request(self):
+        future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
+    
+if __name__ == "__main__":
+    rclpy.init()
+    delete_client = DeleteWalker()
+    response = delete_client.send_request()
+    delete_client.get_logger().info("entity deleted!")
+    delete_client.destroy_node()
+    rclpy.shutdown()
