99dcf1874d8b71b3486ea934f0163cbe4e580c1e
aihoque2 <aihoque2@illinois.edu>
2023-03-27 16:56:09 -0700

    remove unnecessary function in reset_controls

--- walker_ai/src/reset_controls.py
+++ walker_ai/src/reset_controls.py
@@ -9,8 +9,9 @@ from geometry_msgs.msg import Pose
 class ControllerResetClient(Node):
     def __init__(self):
         super().__init__("controller_reset")
+        self.get_logger().info("creating client...")
         self.cli = self.create_client(SwitchController, "/controller_manager/switch_controller")
-        while not self.cli.wait_for_service(timeout_sec=1.0):
+        while not self.cli.wait_for_service():
             self.get_logger().info("service not available, waiting again...")
         self.req = SwitchController.Request()
     
@@ -18,29 +19,26 @@ class ControllerResetClient(Node):
         self.req.activate_controllers = activate_controllers
         self.req.deactivate_controllers = deactivate_controllers
         self.req.activate_asap = True
-        self.req.strictness=1 #BEST_EFFORT=1, STRICT=2
+        self.req.strictness=2 #BEST_EFFORT=1, STRICT=2
         self.future = self.cli.call_async(self.req)
+        self.timeout=100
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
-    
-    def activate_controls(self):
-        result = False
-        controller_arr = ["joint_effort_controller", "joint_state_broadcaster"]
-        result = self.send_request(active_controllers=controller_arr)
-        return result
 
     def reset_controls(self):
         #reset controls from deactivation to activation
+        self.get_logger().info("resetting controls...")
         result = False
         controller_arr = ["joint_effort_controller", "joint_state_broadcaster"]
         deactivate_ok = self.send_request(activate_controllers=[], deactivate_controllers=controller_arr)
         if deactivate_ok:
+            self.get_logger().info("deactivated the controller")
             activate_ok = self.send_request(activate_controllers=controller_arr, deactivate_controllers=[])
             if activate_ok:
                 self.get_logger().info("controllers reset!")
                 result = True
             else:
-                self.get_logger().info("activate ok ==> " + str(activate_ok))
+                self.get_logger().info("activate_ok ==> " + str(activate_ok))
         else:
             self.get_logger().info("DEactivate_ok ==> " + str(deactivate_ok))
 
@@ -50,8 +48,10 @@ class ControllerResetClient(Node):
 if __name__ == "__main__":
     rclpy.init()
     reset_controls = ControllerResetClient()
+    reset_controls.get_logger().info("client created!")
     response = reset_controls.reset_controls()
     reset_controls.get_logger().info("controllers reset!")
+    reset_controls.get_logger().info("here's strictness: "+str(reset_controls.req.strictness))
     reset_controls.destroy_node()
     rclpy.shutdown()
 
--- walker_ai/src/reset_joints.py
+++ walker_ai/src/reset_joints.py
@@ -10,22 +10,42 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import Float64MultiArray
+from std_srvs.srv import Empty
+from controller_manager_msgs.srv import SwitchController
+
+
 
 class JointPub(Node):
     
     def __init__(self):
         super().__init__("joint_reset")
+        
+        # self.sim_cli = self.create_client(Empty, "/reset_sim")
+        # while not self.cli.wait_for_service(timeout_sec=1.0):
+        #     self.get_logger().info("service not available, waiting again...")
+        # self.sim_req = Empty.Request()
+
+        # self.control_cli = self.create_client(SwitchController, "/controller_manager/switch_controller")
+        # while not self.cli.wait_for_service():
+        #     self.get_logger().info("service not available, waiting again...")
+        # self.control_req = SwitchController.Request()
+
         self.pub = self.create_publisher(Float64MultiArray, "joint_effort_controller/commands", 10)
         self.pos = [0.0, 0.0, 0.0, 0.0]
         
-        timer_period = 0.5
+        timer_period = 0.25
         self.timer = self.create_timer(timer_period, self.timer_callback)
     
     def timer_callback(self):
         msg = Float64MultiArray()
         msg.data = array('d', self.pos)
         self.pub.publish(msg)
-
+    
+    def publish_once(self):
+        msg = Float64MultiArray()
+        msg.data = array('d', self.pos)
+        self.pub.publish(msg)
+        rclpy.spin_once(self)
 
 if __name__ == '__main__':
     rclpy.init()
